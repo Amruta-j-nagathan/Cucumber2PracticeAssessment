9b5a94c0b31b2bc387834a1cc186d864ea7413c5
Amruta-j-nagathan <148949541+Amruta-j-nagathan@users.noreply.github.com>
2024-01-13 10:21:19 +0530

    Add files via upload

---
+++ CucumberPracticeAssessment/test-output-thread/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"21682262-606f-4048-88ff-9fc124856cf7","feature":"Test DemoWebShop page I want to use this template for my feature file","scenario":"Testing of LogIn Page","start":1705119540748,"group":1,"content":"","tags":"@all,","end":1705119562893,"className":"passed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
+});
